dc6079a0354d3d2b693209fff7bf990ede603abb
Chris Bui <chrisbui.dev188@gmail.com>
2023-01-06 13:14:06 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import ImageSlider from "./Slider";
 
 const App: React.FC = () => {
-  const images = [
-    "https://picsum.photos/id/1018/1000/600",
-    "https://picsum.photos/id/1015/1000/600",
-    "https://picsum.photos/id/1019/1000/600",
-  ];
-
-  return (
-    <div className="App">
-      <ImageSlider images={images} />
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
